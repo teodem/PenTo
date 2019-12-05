15fa8a78f7dcc98b6098b4cbd7dc501efcc9457a
teodem <matteo.demasi111@gmail.com>
2019-12-05 09:46:18 +0100

    Update pento.sh

--- pento.sh
+++ pento.sh
@@ -9,7 +9,7 @@ echo "Fuzz Scanning"
 echo "starting fuzzing with waf ninja"
 ./wafninja fuzz -u "http://$TARGET/index.php?id=FUZZ" -c "phpsessid=value" -t xss -o output.html
 echo "fuzzing finished"
-echo ""
+echo "═════"
 echo "starting fuzzing.php web dir"
 echo ""
 python3 Module/dirsearch/dirsearch.py -u http://$TARGET -e .php
